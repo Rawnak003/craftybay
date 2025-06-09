40b9de17f15433ae44dd80e24d3d4b6006d1d7c5
Rawnak003 <shmrawnak@gmail.com>
2025-06-09 22:13:41 +0600

    Add #8: Navigation Controller

--- lib/core/app/controller_binder.dart
+++ lib/core/app/controller_binder.dart
@@ -1,9 +1,11 @@
 import 'package:get/get.dart';
+import '../../src/features/onboarding/screens/home/controller/home_nav_controller.dart';
 import '../../src/features/onboarding/screens/parent_screen/controller/main_bottom_nav_bar_controller.dart';
 
 class ControllerBinder extends Bindings {
   @override
   void dependencies() {
     Get.put(MainBottomNavController());
+    Get.put(HomeNavController());
   }
 }
